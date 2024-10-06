509b1fcead91d8a6e868e0f716f99736d73e5ec7
LuckyMan012359 <rorifloris@gmail.com>
2024-10-06 01:05:39 +0100

    Complete project

--- src/app/(dashboard)/components/wagmi.tsx
+++ src/app/(dashboard)/components/wagmi.tsx
@@ -2,14 +2,11 @@ import { http, createConfig } from "wagmi";
 import { base, optimism, sepolia } from "wagmi/chains";
 import { injected, walletConnect } from "wagmi/connectors";
 
-const projectId: string | undefined = process.env.NEXT_PUBLIC_PROJECT_ID;
+const projectId = "803575691fa797c5e26919e9412950f4";
 
 export const config = createConfig({
   chains: [sepolia, optimism, base],
-  connectors: [
-    walletConnect(projectId ? { projectId } : { projectId: "" }),
-    injected(),
-  ],
+  connectors: [walletConnect({ projectId }), injected()],
   transports: {
     [sepolia.id]: http(),
     [optimism.id]: http(),
